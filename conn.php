1a76f42183e6572cc0d3717887d870c56b1493fc
Mona Mohsen Ahmed <129124836+Manonaahmd@users.noreply.github.com>
2023-09-21 12:58:54 +0300

    Addednewphoneindex

---
+++ conn.php
@@ -0,0 +1,42 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "studenntss";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Database connection successfully established!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+try {
+    $sql = "SELECT * FROM   `phone index`";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+if (!empty($rows)) {
+    echo "<table class=`phone index`>";
+    echo "<thead><tr><th>Full Name</th><th>Email</th><th>Action</th></tr></thead>";
+    echo "<tbody>";
+    foreach ($rows as $row) {
+        echo "<tr>";
+        echo "<td>" . $row["FullName"] . "</td>";
+        echo "<td>" . $row["Email"] . "</td>";
+        echo "<td><a href='edit.php?id=" . $row['id'] . "'>Edit</a> | <a href='delete.php?id=" . $row['id'] . "'>Delete</a></td>";
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+} else {
+    echo "No records found in the users table.";
+}
+
+$conn = null;
+?>
